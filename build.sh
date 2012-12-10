4a66bf9bb509499daade9e08d6d9efc21b8a2b12
Andre Miras <andre.miras@gmail.com>
2012-12-10 14:48:27 +0100

    added build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+exec_cmd()
+{
+    echo "Running: $1"
+    # TODO: is that the right way to do it?
+    $1
+    if [ $? != 0 ]
+    then
+        echo "Error while running: $1"
+        post_install_cleaning
+        exit 1
+    fi
+}
+
+exec_cmd "mpic++ master.cpp -o dist/MPIcpp/GNU-Linux-x86/master"
+exec_cmd "mpic++ calculator_slave.cpp -o dist/MPIcpp/GNU-Linux-x86/calculator_slave"
+
+echo "run with:"
+echo "mpirun -np 1 dist/MPIcpp/GNU-Linux-x86/master"
