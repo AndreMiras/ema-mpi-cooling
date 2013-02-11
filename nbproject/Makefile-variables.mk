4c1921b3aaffba841cc6473d0b0cc18a6671c32a
Andre Miras <andre.miras@gmail.com>
2013-02-11 05:28:44 -0800

    netbeans autogenerated

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -9,27 +9,27 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=GNU-Linux-x86
 CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
-CND_ARTIFACT_NAME_Debug=mpicooling
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/mpicooling
+CND_ARTIFACT_NAME_Debug=mpi-cooling
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/mpi-cooling
 CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
-CND_PACKAGE_NAME_Debug=mpicooling.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/mpicooling.tar
+CND_PACKAGE_NAME_Debug=mpi-cooling.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/mpi-cooling.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux-x86
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
-CND_ARTIFACT_NAME_Release=mpicooling
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/mpicooling
+CND_ARTIFACT_NAME_Release=mpi-cooling
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/mpi-cooling
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
-CND_PACKAGE_NAME_Release=mpicooling.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/mpicooling.tar
+CND_PACKAGE_NAME_Release=mpi-cooling.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/mpi-cooling.tar
 # MPIcpp configuration
 CND_PLATFORM_MPIcpp=GNU-Linux-x86
 CND_ARTIFACT_DIR_MPIcpp=dist/MPIcpp/GNU-Linux-x86
-CND_ARTIFACT_NAME_MPIcpp=mpicooling
-CND_ARTIFACT_PATH_MPIcpp=dist/MPIcpp/GNU-Linux-x86/mpicooling
+CND_ARTIFACT_NAME_MPIcpp=mpi-cooling
+CND_ARTIFACT_PATH_MPIcpp=dist/MPIcpp/GNU-Linux-x86/mpi-cooling
 CND_PACKAGE_DIR_MPIcpp=dist/MPIcpp/GNU-Linux-x86/package
-CND_PACKAGE_NAME_MPIcpp=mpicooling.tar
-CND_PACKAGE_PATH_MPIcpp=dist/MPIcpp/GNU-Linux-x86/package/mpicooling.tar
+CND_PACKAGE_NAME_MPIcpp=mpi-cooling.tar
+CND_PACKAGE_PATH_MPIcpp=dist/MPIcpp/GNU-Linux-x86/package/mpi-cooling.tar
 #
 # include compiler specific variables
 #
--- nbproject/Package-Release.bash
+++ nbproject/Package-Release.bash
@@ -12,9 +12,9 @@ CND_DISTDIR=dist
 CND_BUILDDIR=build
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/mpicooling
-OUTPUT_BASENAME=mpicooling
-PACKAGE_TOP_DIR=mpicooling/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/mpi-cooling
+OUTPUT_BASENAME=mpi-cooling
+PACKAGE_TOP_DIR=mpi-cooling/
 
 # Functions
 function checkReturnCode
@@ -59,15 +59,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/mpicooling/bin"
+makeDirectory "${NBTMPDIR}/mpi-cooling/bin"
 copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/mpicooling.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/mpi-cooling.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/mpicooling.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/mpi-cooling.tar *
 checkReturnCode
 
 # Cleanup
