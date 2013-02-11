0bd23ea78a16febca0e721f6a81d2983e7b7ddc0
Andre Miras <andre.miras@gmail.com>
2013-02-11 01:28:13 -0800

    mkdir output directory

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ exec_cmd()
     fi
 }
 
+exec_cmd "mkdir -p dist/MPIcpp/GNU-Linux-x86/"
 exec_cmd "mpic++ -g utils.cpp master.cpp -o dist/MPIcpp/GNU-Linux-x86/master"
 exec_cmd "mpic++ -g utils.cpp calculator_slave.cpp -o dist/MPIcpp/GNU-Linux-x86/calculator_slave"
 
