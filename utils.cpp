83a729ae58515b5f8932657f6c93f3a606737c40
Andre Miras <andre.miras@gmail.com>
2013-02-18 15:22:30 +0100

    generic display array and matrix using templates

--- utils.cpp
+++ utils.cpp
@@ -22,36 +22,3 @@ void create_mpi_calculator_init_type(MPI_Datatype& mpi_calculator_init_type)
 	MPI_Type_commit(&mpi_calculator_init_type);
 }
 
-void display_matrix(const vector<vector<int> > matrix)
-{
-	cout << "{" << endl;
-	for (int row=0; row < matrix.size(); row++)
-	{
-		/*
-		for (int col=0; col < matrix[row].size(); col++)
-		{
-			cout << matrix[row][col] << "\t";
-		}
-		cout << endl;
-		*/
-		cout << "  ";
-		display_array(matrix[row].data(), matrix[row].size());
-	}
-	cout << "}" << endl;
-}
-
-void display_array(const int array[], const int array_size)
-{
-	// cout << "array[" << array_size << "] = ";
-	cout << "{ ";
-	for(int i=0; i<array_size; i++)
-	{
-		// printf("array[%i]: %i\n", i, array[i]);
-		cout << array[i];
-		if (i != (array_size -1))
-		{
-			cout << ", ";
-		}
-	}
-	cout << " }" << endl;
-}
--- utils.h
+++ utils.h
@@ -18,5 +18,35 @@ enum message_type {
 };
 
 void create_mpi_calculator_init_type(MPI_Datatype& mpi_calculator_init_type);
+
 void display_matrix(const vector<vector<int> > matrix);
-void display_array(const int array[], const int array_size);
+
+template <class T>
+void display_array(const T array[], const int array_size)
+{
+	// cout << "array[" << array_size << "] = ";
+	cout << "{ ";
+	for(int i=0; i<array_size; i++)
+	{
+		// printf("array[%i]: %i\n", i, array[i]);
+		cout << array[i];
+		if (i != (array_size -1))
+		{
+			cout << ", ";
+		}
+	}
+	cout << " }" << endl;
+}
+
+
+template <typename T>
+void display_matrix(const vector<vector<T> > matrix)
+{
+	cout << "{" << endl;
+	for (int row=0; row < matrix.size(); row++)
+	{
+		cout << "  ";
+		display_array<T>(matrix[row].data(), matrix[row].size());
+	}
+	cout << "}" << endl;
+}
