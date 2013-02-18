f62a9b34e8e6728ec0260f574a3d36895954eeb5
Andre Miras <andre.miras@gmail.com>
2013-02-18 15:25:32 +0100

    removed deprecated code

--- master.cpp
+++ master.cpp
@@ -482,24 +482,6 @@ int main(int argc, char *argv[])
 		/* creates a type for struct calculator_init */
 		MPI_Datatype mpi_calculator_init_type;
 		create_mpi_calculator_init_type(mpi_calculator_init_type);
-		/*
-		const int nitems = 3;	// neighbours_array, neighbours_array_size and initial_temperature
-		int blocklengths[nitems] = {
-				8,	// each case has 8 neighbours
-				1,
-				1
-		};
-		MPI_Datatype types[nitems] = {MPI_INT, MPI_INT, MPI_INT};
-		MPI_Datatype mpi_calculator_init_type;
-		MPI_Aint offsets[nitems];
-
-		offsets[0] = offsetof(calculator_init, neighbours_array);
-		offsets[1] = offsetof(calculator_init, neighbours_array_size);
-		offsets[2] = offsetof(calculator_init, initial_temperature);
-
-		MPI_Type_create_struct(nitems, blocklengths, offsets, types, &mpi_calculator_init_type);
-		MPI_Type_commit(&mpi_calculator_init_type);
-		*/
 
 		// Sets neighbours_array and initial_temperature
 		calculator_init calculator_init1;
